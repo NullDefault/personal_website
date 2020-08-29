122a19b2086eeeb677ab3ae044fda6a542cafa24
NullDefault <davisha999@gmail.com>
2020-08-29 13:58:35 -0700

    removing screenshot files

--- src/typescript/pages/ProjectGallery.tsx
+++ src/typescript/pages/ProjectGallery.tsx
@@ -1,47 +1,32 @@
 import React from 'react';
 import '../../static/css/pages/ProjectGallery.css';
 
-import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
-import {Paper} from "@material-ui/core";
-
 import Container from '@material-ui/core/Container'
-import aiScreenshot from "../../static/assets/screenshots/ai_screenshot.gif";
-// import beeSimScreenshot from '../../static/assets/screenshots/beesim_screenshot.gif';
-import depthsScreenshot from "../../static/assets/screenshots/depths_screenshot.gif";
-import processingScreenshot from "../../static/assets/screenshots/processing_screenshot.gif";
-import TDDScreenshot from "../../static/assets/screenshots/tdn_screenshot.gif";
-import JSScreenshot from '../../static/assets/screenshots/js_screenshot.gif';
 
 
 const projectsData = [
-    //{
-    //    img: beeSimScreenshot,
-    //    title: 'beeSim',
-    //    description: 'pyGame bee simulation with a focus on agent based modeling.',
-    //},
     {
-        img: aiScreenshot,
+        title: 'beeSim',
+        description: 'pyGame bee simulation with a focus on agent based modeling.',
+    },
+    {
         title: 'AI Projects',
         description: 'Various AI projects and explorations.',
     },
     {
-        img: depthsScreenshot,
         title: 'Depths',
         description: 'pyGame roguelike game.',
     },
     {
-        img: TDDScreenshot,
         title: 'SIR Disease Simulation',
         description: 'SIR Simulation Project for my Data Science Class',
     },
     {
-        img: processingScreenshot,
         title: 'Processing Sketches',
         description: 'A collection of sketches in the Processing language',
     },
     {
-        img: JSScreenshot,
         title: 'Various Web Dev Stuff',
         description: 'My all purpose repository for web development experiments',
     },
