718709653decc0604b3f27ef7f6614c00e77f0c5
NullDefault <davisha999@gmail.com>
2020-11-08 21:15:28 -0500

    day two of work on data viz stuff

--- src/typescript/pages/DataVizProject.tsx
+++ src/typescript/pages/DataVizProject.tsx
@@ -1,44 +1,52 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TableauReport from 'tableau-react';
-import {Container, ListGroup, ListGroupItem} from "react-bootstrap";
-import "../../static/css/components/DataVizProject.css";
+import {Container, ListGroup, ListGroupItem, Col} from "react-bootstrap";
+import "../../static/css/pages/DataVizProject.css";
 
 function DataVizProject() {
     return (
         <Container className="align-items-center">
             <ListGroup className="tableau-report">
                 <ListGroupItem className="tableau-list-item">
-                    <TableauReport
-                        options = {{
-                            hideTabs: false
-                        }}
-                        url="https://public.tableau.com/views/DataVizFinal_16047841003180/WorldSuicideHeatmap?:language=en&:display_count=y&:origin=viz_share_link"
-                    />
+                    <Container>
+                        <TableauReport
+                            options={{
+                                hideTabs: true,
+                            }}
+                            url="https://public.tableau.com/views/DataVizFinal_16047841003180/WorldSuicides?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
+                        />
+                    </Container>
                 </ListGroupItem>
                 <ListGroupItem className="tableau-list-item">
-                    <TableauReport
-                        options = {{
-                            hideTabs: false
-                        }}
-                        url="https://public.tableau.com/views/DataVizFinal_16047841003180/WorldGDPHeatmap?:language=en&:display_count=y&:origin=viz_share_link"
-                    />
+                    <Container>
+                        <TableauReport
+                            options={{
+                                hideTabs: false
+                            }}
+                            url="https://public.tableau.com/views/DataVizFinal_16047841003180/GDPvsHDIHeatmaps?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
+                        />
+                    </Container>
                 </ListGroupItem>
                 <ListGroupItem className="tableau-list-item">
-                    <TableauReport
-                        options = {{
-                            hideTabs: false
-                        }}
-                        url="https://public.tableau.com/views/DataVizFinal_16047841003180/WorldHDIHeatmap?:language=en&:display_count=y&:origin=viz_share_link"
-                    />
+                    <Container>
+                        <TableauReport
+                            options={{
+                                hideTabs: false
+                            }}
+                            url="https://public.tableau.com/views/DataVizFinal_16047841003180/TimeSeries?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
+                        />
+                    </Container>
                 </ListGroupItem>
                 <ListGroupItem className="tableau-list-item">
-                    <TableauReport
-                        options = {{
-                            hideTabs: false
-                        }}
-                        url="https://public.tableau.com/views/DataVizFinal_16047841003180/TrendLines?:language=en&:display_count=y&:origin=viz_share_link"
-                    />
+                    <Container>
+                        <TableauReport
+                            options={{
+                                hideTabs: false
+                            }}
+                            url="https://public.tableau.com/views/DataVizFinal_16047841003180/ScatterplotDb?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
+                        />
+                    </Container>
                 </ListGroupItem>
             </ListGroup>
         </Container>
