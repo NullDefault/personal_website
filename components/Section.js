7ce9b60f078a236fab2fc46015075bb14acb1097
NullDefault <davisha999@gmail.com>
2021-02-16 18:36:09 -0500

    imports fix

--- components/Section.js
+++ components/Section.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Center } from "@chakra-ui/react";
-import Container from "./container";
+import Container from "../components/Container";
 
 const Section = (props) => {
     const { full, children, ...rest } = props;
