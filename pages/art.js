be14742a8eba85c96222e28b36cf997d979cff2a
NullDefault <davisha999@gmail.com>
2021-03-20 18:33:14 -0400

    styling

--- pages/art.js
+++ pages/art.js
@@ -11,13 +11,13 @@ import shuffle from "../functions/shuffle";
 export default function Art() {
   return (
     <PageTransition>
-      <Flex minH="80vh" w="95%" pt="3rem" direction="column">
-        <Wrap align="center" justify="center" px="5vw">
+      <Flex minH="80vh" w={{base: "100%", md: "95%"}} pt="3rem" direction="column">
+        <Wrap align="center" justify="center">
           {shuffle(art).map(
               ({ id, title, image }) => {
                 return (
                       <WrapItem key={id}>
-                        <Image maxW="800px" src={`art/${image}`} alt={title} shadow="dark-lg" bg="white" />
+                        <Image maxW={{base: "350px", md: "800px"}} src={`art/${image}`} alt={title} shadow="dark-lg" bg="white" />
                       </WrapItem>
                 );
               }
