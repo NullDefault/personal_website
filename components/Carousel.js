b099191c2c55e43eadc08dd4a33448b1ca0808a0
NullDefault <davisha999@gmail.com>
2021-09-24 12:58:03 -0400

    silly error - now it works

--- components/Carousel.js
+++ components/Carousel.js
@@ -94,7 +94,7 @@ const Carousel = ({ slides }) => {
                 />
                 <Text
                   fontSize="xs"
-                  display={{ sm: "none", md: "block" }}
+                  display={{ base: "none", md: "block" }}
                   p="8px 12px"
                   pos="absolute"
                   top="0"
