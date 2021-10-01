419cfb2dc240788e57c3df33c2964c1fcecaf199
NullDefault <davisha999@gmail.com>
2021-10-01 12:52:20 -0400

    added social media preview settings

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,18 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <title>NullDefault</title>
+          <meta property="og:url" content="https://www.nulldefault.com/" />
+          <meta property="og:type" content="Portfolio Website" />
+          <meta property="og:title" content="David Nesterov-Rappoport Website" />
+          <meta name="twitter:card" content="David Nesterov-Rappoport's Online Portfolio" />
+          <meta
+            property="og:description"
+            content="Hi, I'm David! I'm a software engineer and designer."
+          />
+          <meta property="og:image" content={"https://www.nulldefault.com/doodles.svg"} />
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
