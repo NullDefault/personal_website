ef5e70fcfa6d78a7413ffaf1475441d56331b907
NullDefault <davisha999@gmail.com>
2021-12-10 17:30:37 -0500

    added filter functionality to project page

--- components/composites/ProjectsList.js
+++ components/composites/ProjectsList.js
@@ -1,53 +1,126 @@
 import {
   Heading,
-  HStack,
+  Flex,
+  Box,
+  IconButton,
+  Tooltip,
   SimpleGrid,
-  GridItem,
   VStack,
-  useBreakpointValue,
+  Spacer,
+  HStack,
 } from "@chakra-ui/react";
+import { useState } from "react";
 import { ProjectCard } from "./ProjectCard";
+import { AiFillDatabase, AiOutlineFolderView } from "react-icons/ai";
+import { SiMoleculer } from "react-icons/si";
+import { BiGame } from "react-icons/bi";
+import { GiBrain } from "react-icons/gi";
+
+function FilterButton({ stateFunc, stateVar, icon, type }) {
+  return (
+    <Tooltip label={"Show projects with the " + type + " tag"}>
+      <IconButton
+        aria-label={"filter " + type}
+        size="lg"
+        icon={icon}
+        colorScheme={stateVar ? "green" : "gray"}
+        onClick={stateFunc}
+      />
+    </Tooltip>
+  );
+}
 
 export function ProjectsList({ projects }) {
+  const [backendFilter, setBackendFilter] = useState(false);
+  const [frontendFilter, setFrontendFilter] = useState(false);
+  const [simulationFilter, setSimulationFilter] = useState(false);
+  const [gameFilter, setGameFilter] = useState(false);
+  const [aiFilter, setAiFilter] = useState(false);
+
+  let filterValues = [
+    ...(backendFilter ? ["Backend"] : []),
+    ...(frontendFilter ? ["Frontend"] : []),
+    ...(simulationFilter ? ["Simulation"] : []),
+    ...(gameFilter ? ["Game"] : []),
+    ...(aiFilter ? ["Artificial Intelligence"] : []),
+  ];
+
   return (
     <VStack spacing={7} alignItems="flex-start">
-      <HStack pt={3}>
-        <Heading as="h2">Projects</Heading>
-      </HStack>
+      <Flex pt={3} w="100%">
+        <Box>
+          <Heading as="h2">Projects</Heading>
+        </Box>
+
+        <Spacer />
+        <HStack spacing={3}>
+          <FilterButton
+            stateFunc={() => setBackendFilter(!backendFilter)}
+            stateVar={backendFilter}
+            icon={<AiFillDatabase />}
+            type="backend"
+          />
+          <FilterButton
+            stateFunc={() => setFrontendFilter(!frontendFilter)}
+            stateVar={frontendFilter}
+            icon={<AiOutlineFolderView />}
+            type="frontend"
+          />
+          <FilterButton
+            stateFunc={() => setSimulationFilter(!simulationFilter)}
+            stateVar={simulationFilter}
+            icon={<SiMoleculer />}
+            type="simulation"
+          />
+          <FilterButton
+            stateFunc={() => setGameFilter(!gameFilter)}
+            stateVar={gameFilter}
+            icon={<BiGame />}
+            type="game"
+          />
+          <FilterButton
+            stateFunc={() => setAiFilter(!aiFilter)}
+            stateVar={aiFilter}
+            icon={<GiBrain />}
+            type="ai"
+          />
+        </HStack>
+      </Flex>
 
       <SimpleGrid spacing={{ base: 4, md: 12 }} columns={{ base: 1, lg: 2 }}>
-        {projects.map(
-          (
-            { id, title, stack, liveUrl, githubUrl, slides, description },
-            i,
-            arr
-          ) => {
-            let breakpoint = useBreakpointValue({ base: false, lg: true });
-            let projectCard = (
-              <ProjectCard
-                key={id}
-                title={title}
-                stack={stack}
-                liveUrl={liveUrl}
-                githubUrl={githubUrl}
-                slides={slides}
-                description={description}
-              />
-            );
+        {projects
+          .filter(({ stack }) => {
 
-            if (breakpoint && arr.length - 1 === i) {
-              return (
-                <GridItem
-                  colSpan={1}
-                  ml="50%"
-                  width="100%"
-                  children={[projectCard]}
+            
+            if (!filterValues.length){
+              return true;
+            }
+            else if (stack.categories.some(value => filterValues.includes(value))){
+              return true;
+            }
+            return false;
+          })
+          .map(
+            (
+              { id, title, stack, liveUrl, githubUrl, slides, description },
+              i,
+              arr
+            ) => {
+              let projectCard = (
+                <ProjectCard
+                  key={"project-#" + id}
+                  title={title}
+                  stack={stack}
+                  liveUrl={liveUrl}
+                  githubUrl={githubUrl}
+                  slides={slides}
+                  description={description}
                 />
               );
+
+              return projectCard;
             }
-            return projectCard;
-          }
-        )}
+          )}
       </SimpleGrid>
     </VStack>
   );
