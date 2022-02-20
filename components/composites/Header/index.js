ba6655c424f598c503d104ad19f86347573ba0c6
NullDefault <davisha999@gmail.com>
2022-02-19 21:57:31 -0500

    added top border

--- components/composites/Header/index.js
+++ components/composites/Header/index.js
@@ -31,43 +31,46 @@ const Header = (props) => {
   const textColor = useColorModeValue("dark", "light");
 
   return (
-    <Flex
-      as="nav"
-      align="center"
-      justify="space-between"
-      wrap="wrap"
-      w="100%"
-      pt={8}
-      px={8}
-      bg="transparent"
-      color={textColor}
-      {...props}
-    >
-      <Flex align="center">
-        <NavLogo w={["60px", "120px"]} />
-      </Flex>
-
-      <Box display={{ base: "block", md: "none" }} onClick={toggleMenu}>
-        {show ? <FaWindowClose /> : <FaBars />}
-      </Box>
-
-      <Box
-        display={{ base: show ? "block" : "none", md: "block" }}
-        pb={{ base: show ? 8 : 0 }}
-        flexBasis={{ base: "100%", md: "auto" }}
+    <>
+      <Flex h="6px" bg="black" />
+      <Flex
+        as="nav"
+        align="center"
+        justify="space-between"
+        wrap="wrap"
+        w="100%"
+        pt={5}
+        px={8}
+        bg="transparent"
+        color={textColor}
+        {...props}
       >
-        <Flex
-          align={["center", "center", "center", "center"]}
-          justify={["center", "space-between", "flex-end", "flex-end"]}
-          direction={["column", "row", "row", "row"]}
-          pt={[4, 4, 0, 0]}
-        >
-          <MenuItems to="/">Home</MenuItems>
-          <MenuItems to="/art">Art</MenuItems>
-          <ColorModeSwitch />
+        <Flex align="center">
+          <NavLogo w={["60px", "120px"]} />
         </Flex>
-      </Box>
-    </Flex>
+
+        <Box display={{ base: "block", md: "none" }} onClick={toggleMenu}>
+          {show ? <FaWindowClose /> : <FaBars />}
+        </Box>
+
+        <Box
+          display={{ base: show ? "block" : "none", md: "block" }}
+          pb={{ base: show ? 8 : 0 }}
+          flexBasis={{ base: "100%", md: "auto" }}
+        >
+          <Flex
+            align={["center", "center", "center", "center"]}
+            justify={["center", "space-between", "flex-end", "flex-end"]}
+            direction={["column", "row", "row", "row"]}
+            pt={[4, 4, 0, 0]}
+          >
+            <MenuItems to="/">Home</MenuItems>
+            <MenuItems to="/art">Art</MenuItems>
+            <ColorModeSwitch />
+          </Flex>
+        </Box>
+      </Flex>
+    </>
   );
 };
 
