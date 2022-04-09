1074d289eb85eafc2a61cef2b3f10a6921941ae0
NullDefault <davisha999@gmail.com>
2022-04-09 17:57:13 -0400

    updated footer icons

--- components/composites/Footer/CryptoStats.js
+++ components/composites/Footer/CryptoStats.js
@@ -4,13 +4,9 @@ export const CryptoStats = ({ currency, data }) => {
   return (
     <HStack spacing="12px" p={4}>
       <Box>
-        <Image
-          w={["8vw", "4vw"]}
-          src={`/${currency}.png`}
-          style={{ WebkitFilter: "invert(1)", filter: "invert(1)" }}
-        />
+        <Image w={["8vw", "4vw"]} src={`/${currency}.png`} />
       </Box>
-      <VStack>
+      <VStack pl={2}>
         <Badge colorScheme="green">Buy: {data.buy}</Badge>
         <Badge colorScheme="red">Sell: {data.sell}</Badge>
       </VStack>
