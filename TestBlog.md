8bf7797f6a09885fd7335ee0f4964a4a3bdb220e
NullDefault <davisha999@gmail.com>
2022-06-11 17:34:26 -0400

    custom component renders for markdown

---
+++ TestBlog.md
@@ -0,0 +1,43 @@
+# Heading 1 - Exciting Title!  \n
+> Blockquote about something very fascinating \n
+
+- Heraclitus, the Man Himself \n
+
+---\n
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. \n
+
+![image](https://www.rollingstone.com/wp-content/uploads/2022/04/drain-gang.jpg)\n
+
+## Subheading! \n
+
+1. Wowza \n
+2. Bowza \n
+3. hooza! \n
+\n
+
+### Note This! \n
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \n
+
+#### Also Important \n
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. \n
+
+```js
+const Testing() => <Gorgus />;
+``` 
+\n
+
+- Who knew? \n
+- Perhaps I did! \n
+- But then, again, who knows? \n
+
+## Whoa, another whole thing? \n
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. \n
+
+###### No Way! \n
+
+> Borgus Dorgus horgus! \n
+
+
--- components/Post.js
+++ components/Post.js
@@ -5,6 +5,10 @@ import {
   Text,
   Stack,
   useColorModeValue,
+  Code,
+  Divider,
+  Image,
+  Link,
 } from "@chakra-ui/react";
 import ReactMarkdown from "react-markdown";
 
@@ -12,10 +16,12 @@ export default function Post({ blog }) {
   const parseFirebaseMarkdownString = (str) => {
     return str.replace(/\\n/g, "\n");
   };
+
   return (
     <Center py={6}>
       <Box
-        minW={"445px"}
+        minW={"400px"}
+        maxW="95vw"
         w={"full"}
         bg={useColorModeValue("white", "gray.900")}
         boxShadow={"2xl"}
@@ -24,15 +30,116 @@ export default function Post({ blog }) {
         overflow={"hidden"}
       >
         <Stack>
-          <Heading color={"green.500"} fontSize={"5xl"} letterSpacing={1.5}>
+          <Heading fontSize={"6xl"} letterSpacing={1.5}>
             {blog.title}
           </Heading>
+          <Divider py={4} />
           <ReactMarkdown
             children={parseFirebaseMarkdownString(blog.md_content)}
+            components={{
+              ul: ({ node, ...props }) => (
+                <Box pl="18px" display="block" {...props} />
+              ),
+              ol: ({ node, ...props }) => (
+                <Box
+                  pl="18px"
+                  display="block"
+                  listStyleType="decimal"
+                  {...props}
+                />
+              ),
+              em: ({ node, ...props }) => <Text as="em" {...props} />,
+              strong: ({ node, ...props }) => <Text as="strong" {...props} />,
+              h1: ({ node, ...props }) => (
+                <Heading
+                  as="h1"
+                  display="block"
+                  size="2xl"
+                  color="red.300"
+                  {...props}
+                />
+              ),
+              h2: ({ node, ...props }) => (
+                <Heading
+                  as="h2"
+                  display="block"
+                  size="xl"
+                  color="orange.300"
+                  {...props}
+                />
+              ),
+              h3: ({ node, ...props }) => (
+                <Heading
+                  as="h3"
+                  display="block"
+                  size="lg"
+                  color="green.300"
+                  {...props}
+                />
+              ),
+              h4: ({ node, ...props }) => (
+                <Heading
+                  as="h4"
+                  display="block"
+                  size="md"
+                  color="blue.300"
+                  {...props}
+                />
+              ),
+              h5: ({ node, ...props }) => (
+                <Heading
+                  as="h5"
+                  display="block"
+                  size="sm"
+                  color="blueviolet.300"
+                  {...props}
+                />
+              ),
+              h6: ({ node, ...props }) => (
+                <Heading
+                  as="h6"
+                  display="block"
+                  size="xs"
+                  color="purple.300"
+                  {...props}
+                />
+              ),
+              p: ({ node, ...props }) => (
+                <Text
+                  style={{
+                    display: "block",
+                    marginBlockStart: "1em",
+                    marginBlockEnd: "1em",
+                    marginInlineStart: "0px",
+                    marginInlineEnd: "0px",
+                    lineHeight: "175%",
+                  }}
+                  {...props}
+                />
+              ),
+              a: ({ node, ...props }) => <Link {...props} />,
+              code: ({ node, ...props }) => <Code {...props} />,
+              img: ({ node, ...props }) => <Image {...props} />,
+              blockquote: ({ node, ...props }) => (
+                <Box
+                  style={{
+                    fontStyle: "italic",
+                    display: "block",
+                    marginBlockStart: "1em",
+                    marginBlockEnd: "1em",
+                    borderLeft: "5px solid #ccc",
+                    padding: "0.5em 10px 0.5em 10px",
+                  }}
+                  {...props}
+                />
+              ),
+            }}
           />
         </Stack>
         <Stack direction={"column"} spacing={0} fontSize={"sm"}>
-          <Text color={"gray.500"}>{blog.added.toDate().toUTCString()}</Text>
+          <Text color={"gray.500"}>
+            NullDefault - {blog.added.toDate().toUTCString()}
+          </Text>
         </Stack>
       </Box>
     </Center>
